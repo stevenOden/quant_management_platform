d010b9dc6be52f73053e6bb9a1727fea0aba0883
Steven Oden <steve.oden14@gmail.com>
2025-12-26 16:27:27 -0500

    adding systemstate table and supporting logic to disable trading if portfolio becomes disparate from execution service

--- services/execution_service/app/models/trade.py
+++ services/execution_service/app/models/trade.py
@@ -7,5 +7,13 @@ class Trade(SQLModel, table=True):
     quantity: float
     price: float
     side: str # "BUY" or "SELL"
-
     timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+class SystemState(SQLModel, table = True):
+    '''Table to store execution service system state for recording whether
+    the service is out of sync with the portfolio service due to a failed update attempt.
+    If the service is our of sync, the portfolio_sync_required falg will be true.
+    The execution service will be restricted from executing any further trades until
+    a portfolio re-sync is performed and this flag is reset to False'''
+    id: int = Field(default=1, primary_key=True)
+    portfolio_sync_required: bool = Field(default=False)
